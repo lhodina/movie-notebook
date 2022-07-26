51d10bdf15db4f7404d432c210638fffee04563f
luke <lhodina@gmail.com>
2022-07-26 12:40:35 -0500

    create director page and begin favorite functionality

--- routes/directors.js
+++ routes/directors.js
@@ -3,7 +3,7 @@ const { check, validationResult } = require("express-validator");
 
 const { csrfProtection, asyncHandler } = require("../utils");
 const db = require("../db/models");
-const { Director } = db;
+const { Director, Movie, DirectorFavorite } = db;
 const { requireAuth } = require("../auth");
 
 const router = express.Router();
@@ -39,6 +39,67 @@ router.post("/", validateDirector, csrfProtection, asyncHandler(async (req, res)
         name
     });
     res.redirect("/directors");
+}));
+
+
+router.get("/:id", csrfProtection, asyncHandler(async (req, res) => {
+    const directorId = parseInt(req.params.id, 10);
+    const director = await Director.findByPk(directorId);
+    const directors = await Director.findAll();
+    const movies = await Movie.findAll();
+    const directedMovies = await Movie.findAll({ where: { directorId } } );
+    const favoriteMovies = await DirectorFavorite.findAll({ where: { directorId }});
+
+    let years = [];
+    let today = new Date().getFullYear();
+    for (let i = 1888; i < today + 1; i++) {
+        years.push(i);
+    }
+
+    console.log("years:", years);
+
+    res.render("director", {
+        director,
+        directors,
+        movies,
+        directedMovies,
+        favoriteMovies,
+        years,
+        csrfToken: req.csrfToken()
+    });
+}));
+
+
+router.post("/:id/favorites/add", csrfProtection, asyncHandler(async (req, res) => {
+    const { selectFavorite } = req.body;
+    let movie;
+
+    const {
+        title,
+        directorId,
+        yearReleased,
+        imageLink
+    } = req.body;
+
+    if (selectFavorite) {
+        movie = await Movie.findOne({ where: { title: selectFavorite } });
+    } else {
+        movie = await Movie.create({
+            title,
+            directorId,
+            yearReleased,
+            imageLink
+        });
+    }
+
+    const current_director_id = parseInt(req.params.id, 10);
+
+    const directorFavorite = await DirectorFavorite.create({
+        directorId: current_director_id,
+        movieId: movie.id,
+    });
+
+    res.redirect("/");
 }));
 
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("index", {
-        title: "Splish-splash Page"
+        title: "INT. MOVIE NOTEBOOK OFFICES -- DAY"
     });
 });
 
