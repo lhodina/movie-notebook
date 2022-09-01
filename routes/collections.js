5135ea65f08fb6cd3b7a62ac84c51434d3097022
luke <lhodina@gmail.com>
2022-09-01 10:32:15 -0500

    Fix radio buttons for populated edit form

--- routes/collections.js
+++ routes/collections.js
@@ -124,6 +124,7 @@ router.get("/:id", csrfProtection, asyncHandler(async (req, res) => {
 
 
 router.post("/:id", csrfProtection, asyncHandler(async (req, res) => {
+    console.log("*****req.body:", req.body);
     const { userId } = req.session.auth;
     const collectionId = parseInt(req.params.id, 10);
     const { selectMovie } = req.body;
